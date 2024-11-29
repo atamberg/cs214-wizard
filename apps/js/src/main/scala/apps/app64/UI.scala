d2515d3219c1e7b2caed8df20a887a61c54429cc
Mattia <mattia.metzler@epfl.ch>
2024-11-29 17:27:31 +0100

    UI: Add first (very basic) UI, to complete scaffold

--- apps/js/src/main/scala/apps/app64/UI.scala
+++ apps/js/src/main/scala/apps/app64/UI.scala
@@ -21,8 +21,16 @@ object UI extends WSClientApp:
 class Instance(userId: UserId, sendMessage: ujson.Value => Unit, target: dom.Element)
   extends WebClientAppInstance[Event, View](userId, sendMessage, target):
 
-  override val wire: AppWire[Event, View] = ???
+  override val wire: AppWire[Event, View] = Wire
 
-  override def css: String = ???
+  override def css: String = super.css + 
+    """
+    | .app64 .h2 {
+    |   padding: 1rem;
+    | }
+    """.stripMargin
 
-  override def render(userId: UserId, view: View): all.Frag = ???
+  override def render(userId: UserId, view: View): Frag = 
+    frag(
+      h2(b("Wizards: "), "It's basically gambling Jass")
+    )
