1d6619fe4b685db16699b3f6a4988f446d5c0d59
Anthony Tamberg <anthony.tamberg@epfl.ch>
2024-11-29 09:54:44 +0100

    Types: Add initial types
    includes Event, View, Phase, State, and other types required for the
    game to eventually function correctly

--- apps/shared/src/main/scala/apps/app64/types.scala
+++ apps/shared/src/main/scala/apps/app64/types.scala
@@ -1,5 +1,43 @@
 package apps.app64
 
-type Event = Unit
-type State = Unit
-type View  = Unit
+import cs214.webapp.UserId
+
+case class Stake(tricks: Int, bets: Int)
+
+enum Suit:
+  case Hearts, Diamonds, Clubs, Spades
+
+case class Card(suit: Suit, value: Int):
+  override def toString =
+    s"$value of ${suit}"
+
+type Hand = Set[Card]
+type IdMap[T] = Map[UserId, T]
+
+case class State(
+  players: Vector[UserId],
+  stakes: IdMap[Stake],
+  hands:  IdMap[Hand],
+  scores: IdMap[Int],
+  trumpSuit: Suit,
+  currentSuit: Suit,
+  round: Int,
+  phase: Phase
+)
+
+enum Phase:
+  case Bet, Play, RoundEnd, GameEnd
+
+enum Event:
+  case AnnounceBet(bet: Int)
+  case PlayCard(card: Card)
+
+case class View(
+  phaseView: PhaseView,
+  scoreView: IdMap[Int]
+)
+
+enum PhaseView:
+  case CardSelecting(hand: Hand)
+  case BetSelecting(bet: Int)
+  case Waiting(ready: IdMap[Boolean])
