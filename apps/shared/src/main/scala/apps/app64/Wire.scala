ae3ae43afd3924ccf4e58d92dd10aae2c0b7065a
Ali Gorgani <aligorgani2006@gmail.com>
2024-11-29 20:18:34 +0100

    Integration: Implement preliminary backbone for (De)Serialization
    Implement the first steps of using upickle for serialization of Phases,
    Views, and Events. Import dependancy library to build.sbt

--- apps/shared/src/main/scala/apps/app64/Wire.scala
+++ apps/shared/src/main/scala/apps/app64/Wire.scala
@@ -2,16 +2,40 @@ package apps.app64
 
 import cs214.webapp.*
 import scala.util.{Failure, Success, Try}
+import upickle.*;
+import upickle.default._
 
 object Wire extends AppWire[Event, View]:
   import ujson.*
+  import upickle.default.*
+  import Event.* 
+  import View.*
+
+  // Provide implicit reads/writes for types
+  implicit val suitRW: ReadWriter[Suit] = macroRW   // Automatically generate for enums
+  implicit val cardRW: ReadWriter[Card] = macroRW  // Automatically generate for case classes
+  implicit val stakeRW: ReadWriter[Stake] = macroRW
+  implicit val phaseRW: ReadWriter[Phase] = macroRW
+  implicit val phaseViewRW: ReadWriter[PhaseView] = macroRW
+  implicit val userIdRW: ReadWriter[UserId] = macroRW
+
+  // IdMap requires explicit conversion since it's a type alias
+  implicit def idMapRW[T: ReadWriter]: ReadWriter[IdMap[T]] =
+    implicitly[ReadWriter[Map[UserId, T]]].bimap[IdMap[T]](
+      _.asInstanceOf[Map[UserId, T]], // Serialization
+      _.asInstanceOf[IdMap[T]]        // Deserialization
+    )
+
+  implicit val eventRW: ReadWriter[Event] = macroRW
+  implicit val viewRW: ReadWriter[View] = macroRW
 
   override object eventFormat extends WireFormat[Event]:
-    override def encode(event: Event): Value = ???
-
-    override def decode(js: Value): Try[Event] = ???
+    override def encode(event: Event): Value = writeJs(event)
+    override def decode(js: Value): Try[Event] = Try(read[Event](js))
 
   override object viewFormat extends WireFormat[View]:
-    override def encode(v: View): Value = ???
+    override def encode(v: View): Value = writeJs(v)
+    override def decode(js: Value): Try[View] = Try(read[View](js))
 
-    override def decode(js: Value): Try[View] = ???
+
+  
--- build.sbt
+++ build.sbt
@@ -22,6 +22,7 @@ lazy val app = (crossProject(JVMPlatform, JSPlatform) in file("./apps"))
   ).settings(
     libraryDependencies ++= Seq(
       "com.lihaoyi" %%% "ujson" % "3.3.1",
+      "com.lihaoyi" %% "upickle" % "3.3.1"
     )
   ).jsSettings(
     scalaJSUseMainModuleInitializer := true,
