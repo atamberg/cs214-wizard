cc90612337ce6f498f1c1eae01ac326238d74509
Anthony Tamberg <anthony.tamberg@epfl.ch>
2024-12-04 23:27:12 +0100

    Backend: Implement bid and play logic
    Fairly rough implementation of Bid and Play. Player should be able to
    place a bid and once all players do so the game moves on to the 'play'
    state.
    Once the game reaches the first player without any cards, we assume that
    all players have no more cards (should always be the case but we should
    check later) and the state moves to the 'round end' phase, where it must
    calculate the winners and losers.

--- apps/jvm/src/main/scala/apps/app64/Logic.scala
+++ apps/jvm/src/main/scala/apps/app64/Logic.scala
@@ -34,6 +34,36 @@ class Logic extends StateMachine[Event, State, View]:
   override def transition(state: State)(
     userId: UserId,
     event: Event
-  ): Try[Seq[Action[State]]] = ???
+  ): Try[Seq[Action[State]]] = Try:
+    import Phase.*
+    import Event.*
+    import Action.*
+
+    state.phase match
+      case Bid =>
+        event match
+          case AnnounceBid(bid) =>
+            val nextBidState = state.placeBid(userId, bid)
+            if nextBidState.allReady then
+              Seq(
+                Render(nextBidState),
+                Pause(1000),
+                Render(nextBidState.nextPhase(Play))
+              )
+            else Seq(Render(nextBidState))
+          case _ => throw IllegalMoveException("You must bid during the bidding phase!")
+      case Play =>
+        event match
+          case PlayCard(card) =>
+            val nextPlayState = state.playCard(userId, card).nextPlayer
+            if state.isHandEmpty(userId) then
+              Seq:
+                Render(nextPlayState.nextPhase(RoundEnd))
+            else
+              Seq:
+                Render(nextPlayState)
+          case _ => throw IllegalMoveException("You must play a card during the playing phase!")
+      case RoundEnd => ???
+      case GameEnd => ???
 
   override def project(state: State)(userId: UserId): View = ???
