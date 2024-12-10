c35a094c52182d9fe16177c662ac4e87fbf8a400
Leoluca Bernardi <leoluca.bernardi@epfl.ch>
2024-12-10 18:38:05 +0100

    Testing: Fixes tests based on changes that occured in the mean time

---
+++ apps/jvm/src/test/scala/apps/app64/EventTest.scala
@@ -0,0 +1,10 @@
+package apps.app64
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+import upickle.default.*
+
+class EventTest extends AnyFlatSpec with Matchers {
+
+
+
+}
--- apps/jvm/src/test/scala/apps/app64/GameStateTest.scala
+++ apps/jvm/src/test/scala/apps/app64/GameStateTest.scala
@@ -18,7 +18,7 @@ class GameStateTest extends AnyFlatSpec with Matchers:
 
   "Card" should "have a correct toString representation" in {
     val card = Card(Suit.Hearts, 10)
-    card.toString shouldBe "10 of Hearts"
+    card.toString shouldBe "🂺"
   }
 
   // Test Event Serialization
@@ -37,38 +37,25 @@ class GameStateTest extends AnyFlatSpec with Matchers:
     valueRead shouldBe playEvent
   }
 
-  // Test State Creation and Manipulation
-  "State" should "create initial game state correctly" in {
-    val players = Vector("player1", "player2")
-    val initialState = State(
-      players = players,
-      stakes = players.map(p => (p, Stake(0, 0))).toMap,
-      cardsPlayed = players.map(p => (p, Card(Suit.None, 0))),
-      hands = players.map(p => (p, Set.empty[Card])).toMap,
-      scores = players.map(p => (p, 0)).toMap,
-      trumpSuit = Suit.Hearts,
-      currentSuit = Suit.Hearts,
-      round = 1,
-      phase = Phase.Bid
-    )
-
-    initialState.players.size shouldBe 2
-    initialState.phase shouldBe Phase.Bid
-    initialState.round shouldBe 1
-  }
 
   // Test View Serialization
   "View" should "serialize and deserialize correctly" in {
-    val hand = Set(Card(Suit.Hearts, 10), Card(Suit.Spades, 5))
+    val validHand = for c <- Set(Card(Suit.Hearts, 10), Card(Suit.Spades, 5)) yield (c, true)
     val stakes = Map(
       "player1" -> Stake(2, 1),
       "player2" -> Stake(3, 2)
     )
 
     val view = View(
-      phaseView = PhaseView.CardSelecting(hand, stakes),
-      scoreView = Map("player1" -> 10, "player2" -> 15)
-    )
+      phaseView = PhaseView.CardSelecting(validHand),
+      scoreView = Map("player1" -> 10, "player2" -> 15),
+      stateView = StateView(
+        Vector("player1", "player2"),
+        Map(),
+        Vector(),
+        Suit.None,
+        Suit.None,
+        0))
 
     val written = write(view)
     val valueRead = read[View](written)
@@ -78,18 +65,12 @@ class GameStateTest extends AnyFlatSpec with Matchers:
   // Test PhaseView Transitions
   "PhaseView" should "support different phase views" in {
     val cardSelecting = PhaseView.CardSelecting(
-      Set(Card(Suit.Hearts, 10)),
-      Map("player1" -> Stake(1, 1))
+      Set((Card(Suit.Hearts, 10), false))
     )
     
-    val betSelecting = PhaseView.BidSelecting(3)
+    val betSelecting = PhaseView.BidSelecting(Set(Card(Suit.Hearts, 10)))
     
-    val waiting = PhaseView.Waiting(
-      Map(
-        "player1" -> true, 
-        "player2" -> false
-      )
-    )
+    val waiting = PhaseView.Waiting(Set(Card(Suit.Hearts, 10)))
 
     // Verify serialization for each phase view
     Seq(cardSelecting, betSelecting, waiting).foreach { phaseView =>
@@ -114,19 +95,16 @@ class GameStateTest extends AnyFlatSpec with Matchers:
   val stake2 = Stake(5, 6)
   val stakeView1 = Map(USER_IDS(0) -> stake0, USER_IDS(1) -> stake1, USER_IDS(2) -> stake2)
   val scoreView1 = Map(USER_IDS(0) -> 2, USER_IDS(1) -> 3, USER_IDS(2) -> 1)
+  val state1 = State.defaultState(USER_IDS)
+  val stateView1 = StateView(
+    USER_IDS.toVector, 
+    state1.stakes,
+    state1.cardsPlayed,
+    state1.trumpSuit,
+    state1.currentSuit,
+    state1.round
+    )
 
-
-  "Different views" should "not be equal" in {
-    val v1 = View(PhaseView.Waiting(Map(USER_IDS(0) -> false)), Map(UID0 -> 10))
-    val v2 = View(PhaseView.Waiting(Map(USER_IDS(0) -> true)), Map(UID0 -> 10))
-    v1 should not be v2
-  }
-
-  "Different events" should "not be equal" in {
-    val e1 = Event.AnnounceBid(10)
-    val e2 = Event.AnnounceBid(20)
-    e1 should not be e2
-  }
 
   "Event wire" should "work correctly" in {
     val events = List(
@@ -142,14 +120,21 @@ class GameStateTest extends AnyFlatSpec with Matchers:
 
   "View wire" should "work correctly" in {
     val views = List(
-      View(PhaseView.Waiting(Map(USER_IDS(0) -> false)), scoreView1),
-      View(PhaseView.CardSelecting(hand1, stakeView1), scoreView1),
-      View(PhaseView.CardSelecting(hand2, stakeView1), scoreView1)
+      View(PhaseView.Waiting(hand0), scoreView1, stateView1),
+      View(PhaseView.CardSelecting(hand1.map(c => (c, c.value % 2 == 0))), scoreView1, stateView1),
+      View(PhaseView.CardSelecting(hand0.map(c => (c, c.value % 2 == 0))), scoreView1, stateView1)
     )
     import apps.app64.Wire.viewFormat._
     views.foreach { view =>
       decode(encode(view)).get shouldBe view
     }
   }
+
+  "Stake" should "score as intended" in {
+    stake0.score shouldBe -30
+    stake1.score shouldBe -10
+    stake2.score shouldBe -10
+  }
+
 end GameStateTest
 
