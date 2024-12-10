0f1f9b5bf696170b8ec5773635160bd60b72bc18
Leoluca Bernardi <leoluca.bernardi@epfl.ch>
2024-12-10 18:38:05 +0100

    Testing: Fixes tests based on changes that occured in the mean time

--- apps/jvm/src/test/scala/apps/app64/CardTest.scala
+++ apps/jvm/src/test/scala/apps/app64/CardTest.scala
@@ -6,40 +6,40 @@ import upickle.default.*
 class CardTest extends AnyFlatSpec with Matchers {
   "scoreAgainst" should "return 0 if the current suit is different from the trump suit and loses the round" in {
     val card = Card(Suit.Hearts, 10)
-    val others = List(Card(Suit.Diamonds, 5), Card(Suit.Clubs, 8), Card(Suit.Spades, 3))
+    val others = Vector(Card(Suit.Diamonds, 5), Card(Suit.Clubs, 8), Card(Suit.Spades, 3))
     val result = card.scoreAgainst(others, trump = Suit.Spades, current = Suit.Diamonds)
     result should be(0)
   }
 
   it should "return 1 if the current suit matches the trump suit and wins the round" in {
     val card = Card(Suit.Hearts, 10)
-    val others = List(Card(Suit.Hearts, 5), Card(Suit.Clubs, 8), Card(Suit.Spades, 3))
+    val others = Vector(Card(Suit.Hearts, 5), Card(Suit.Clubs, 8), Card(Suit.Spades, 3))
     val result = card.scoreAgainst(others, trump = Suit.Hearts, current = Suit.Hearts)
     result should be(1)
   }
   
   it should "return 0 if the current suit matches the trump suit and loses the round" in {
     val card = Card(Suit.Hearts, 5)
-    val others = List(Card(Suit.Hearts, 10), Card(Suit.Clubs, 8), Card(Suit.Spades, 3))
+    val others = Vector(Card(Suit.Hearts, 10), Card(Suit.Clubs, 8), Card(Suit.Spades, 3))
     val result = card.scoreAgainst(others, trump = Suit.Hearts, current = Suit.Hearts)
     result should be(0)
   }
 
   it should "return 0 if the card's suit is different from the current suit and the trump suit and loses the round" in {
     val card = Card(Suit.Diamonds, 10)
-    val others = List(Card(Suit.Hearts, 5), Card(Suit.Clubs, 8), Card(Suit.Spades, 3))
+    val others = Vector(Card(Suit.Hearts, 5), Card(Suit.Clubs, 8), Card(Suit.Spades, 3))
     val result = card.scoreAgainst(others, trump = Suit.Hearts, current = Suit.Clubs)
     result should be(0)
   }
   it should "return 0 if the card's suit matches the current suit and loses the round" in {
     val card = Card(Suit.Clubs, 10)
-    val others = List(Card(Suit.Hearts, 5), Card(Suit.Clubs, 8), Card(Suit.Spades, 3))
+    val others = Vector(Card(Suit.Hearts, 5), Card(Suit.Clubs, 8), Card(Suit.Spades, 3))
     val result = card.scoreAgainst(others, trump = Suit.Hearts, current = Suit.Clubs)
     result should be(0)
   }
   it should "return 1 if the card's suit matches the current suit and wins the round" in {
     val card = Card(Suit.Clubs, 10)
-    val others = List(Card(Suit.Spades, 5), Card(Suit.Clubs, 8), Card(Suit.Spades, 3))
+    val others = Vector(Card(Suit.Spades, 5), Card(Suit.Clubs, 8), Card(Suit.Spades, 3))
     val result = card.scoreAgainst(others, trump = Suit.Hearts, current = Suit.Clubs)
     result should be(1)
   }
