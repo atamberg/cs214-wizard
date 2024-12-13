b15af62fa94e9285a2988fee03f2cdd040b1e370
Leoluca Bernardi <leoluca.bernardi@epfl.ch>
2024-12-13 15:12:01 +0100

    State: Fixed a validity bug
    - State.scala: crucial "!" was missing in condition for all true

--- apps/shared/src/main/scala/apps/app64/State.scala
+++ apps/shared/src/main/scala/apps/app64/State.scala
@@ -239,7 +239,7 @@ case class State(
       yield
         (card, isValid(card)))
     if !validCards.exists((card, valid)
-        => valid && card.suit == currentSuit && (card.isWizard || card.isJester)
+        => valid && card.suit == currentSuit && !(card.isWizard || card.isJester)
       ) then
       hands(userId).map((_, true))
     else validCards
